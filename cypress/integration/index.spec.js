d015c618e8052e095e74e7b341a412f705df7fb0
wataryooou <ryou.asia@gmail.com>
2020-11-29 19:33:43 +0900

    feat: cypressを追加

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+context("/", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("アクセスしたURLがあっている", () => {
+    cy.location("pathname", { timeout: 10000 }).should("include", "/");
+  });
+
+  it("ページが表示されている", () => {
+    cy.get("p").contains("Welcome to page!");
+  });
+});
