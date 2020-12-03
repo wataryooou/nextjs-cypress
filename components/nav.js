6321ec229f29011b576bba348237fa2e6e60c33a
wataryooou <ryou.develop@gmail.com>
2020-12-03 10:10:06 +0900

    test: navのテスト追加

--- components/nav.js
+++ components/nav.js
@@ -5,27 +5,27 @@ export default function Nav() {
   return (
     <nav className={style.nav}>
       <ul>
-        <li>
+        <li data-cy="Home">
           <Link href="/">
             <a>Home</a>
           </Link>
         </li>
-        <li>
+        <li data-cy="About">
           <Link href="/about">
             <a>About</a>
           </Link>
         </li>
-        <li>
+        <li data-cy="Posts">
           <Link href="/posts">
             <a>Posts</a>
           </Link>
         </li>
-        <li>
+        <li data-cy="Contact">
           <Link href="/contact">
             <a>Contact</a>
           </Link>
         </li>
-        <li>
+        <li data-cy="Sample">
           <Link href="/sample">
             <a>Sample</a>
           </Link>
--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -12,4 +12,21 @@ context("/ - Home", () => {
   it("ページが表示されている", () => {
     cy.get("p").contains("Welcome to page!");
   });
+
+  it("全てのページに遷移できる", () => {
+    cy.get("[data-cy='About']").click();
+    cy.location("pathname", { timeout: 10000 }).should("include", "/about");
+
+    cy.get("[data-cy='Posts']").click();
+    cy.location("pathname", { timeout: 10000 }).should("include", "/posts");
+
+    cy.get("[data-cy='Contact']").click();
+    cy.location("pathname", { timeout: 10000 }).should("include", "/contact");
+
+    cy.get("[data-cy='Sample']").click();
+    cy.location("pathname", { timeout: 10000 }).should("include", "/sample");
+
+    cy.get("[data-cy='Home']").click();
+    cy.location("pathname", { timeout: 10000 }).should("include", "/");
+  });
 });
